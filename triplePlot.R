a5fda01cb77290871c5ed449af64363984e749d6
guangchun <hanguangchun@gmail.com>
2017-02-13 16:18:48 -0600

    plot triples of interest

--- triplePlot.R
+++ triplePlot.R
@@ -1,10 +1,10 @@
-tripmPlot <- function(m,e,t,fname='tmp.pdf',xlab='Sample index',ylab='Log2(RPKM)',points.cex=.6,) {
+triPlot <- function(m,e,t,fname='tmp.pdf',xlab='Sample index',ylab='Log2(RPKM)',points.cex=.6) {
     ord <- order(m,decreasing=F)
     m <- m[ord]
     e <- e[ord]
     t <- t[ord]
     ns <- length(m)
-    colfunc <- colorRampPamte(c("green", "red"))
+    colfunc <- colorRampPalette(c("green", "red"))
     dat <- data.frame(x=1:length(m),lncRNA=m,TF=e,gene=t,cols=colfunc(ns))
     ylim <- c(min(dat[,2:4]),max(dat[,2:4]))
     pdf(fname)
@@ -33,6 +33,23 @@ tripmPlot <- function(m,e,t,fname='tmp.pdf',xlab='Sample index',ylab='Log2(RPKM)
                     'LncRNA'),
            pch=c(6,5,20)
            )
+    q25 <- quantile(m,c(.25,.75))
+    n25_l <-sum(m < q25[1])
+    n25_h <-sum(m > q25[2])
+    n50 <- length(m)-n25_h-n25_l
+
+    cols <- c(rep('green',n25_l),rep('grey',length(m)-n25_h-n25_l),rep('red',n25_h))
+    plot(dat$TF,dat$gene,col=cols,pch=20,cex=points.cex,
+         xlab='TF expression',
+         ylab='Gene expression')
+    legend('bottomright',
+           col=c('red','green'),
+           legend=c('LncRNA highExp group',
+                    'LncRNA lowExp group'),
+           pch=20
+           )
+
+
     dev.off()
 }
 
